b0b0c8c8eaaee82b03f7f973349a4ea63f29264d
PramodAB123 <pramodbkl111@gmail.com>
2025-08-30 16:10:10 +0530

    .

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express=require('express');
 const morgan = require('morgan');
+const connectdb = require('./models/db');
 const app=express();
 
+
+connectdb();
 app.use(morgan('dev'))
 app.use(express.json())
 
@@ -10,7 +13,7 @@ app.get('/',(req,res)=>{
    return res.status(200).send("<h1>hello</h1>");
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT 
 app.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`);
 })
