567326fc303b8170823de6d4025e2aa351da5b73
PramodAB123 <pramodbkl111@gmail.com>
2025-08-30 22:22:01 +0530

    connection

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    userName:{
+        type:String,
+        required:[true,'the username is required']
+    },
+    email:{
+        type:String,
+        required:[true,'the email is required']
+    },
+    password:{
+        type:String,
+        required:[true,'the passowrd required']
+    },
+    address:{
+        type:Array
+    },
+    Phone:{
+        type:Number,
+        required:[true,'the number required']
+    },
+    usertype:{
+        type:String,
+        required:[true,'the usertype is required'],
+        default:'client',
+        enum:['client','admin','vendor','driver']
+    },
+    profile:{
+        type:String,
+        default:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9IUW9QQRx9V81XVtoatQ-Qq-tKh1yN0ftYA&s'
+    },
+},{timestamps:true})
+
+module.exports=mongoose.model("restuarent_user",userSchema);
